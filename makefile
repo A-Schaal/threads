62bc4591bdaddb21e3caaaa881f676b6209a27a5
Joy Kitson <tkitson@udel.edu>
2019-04-28 16:49:06 -0400

    reworked switch into two seperate functions: halt and run_next

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CC = gcc
 
 .PHONY:all
-all: thread test01 test01x
+all: test01 test01x
 
 linked_list.o: linked_list.c linked_list.h
 	$(CC) -g -c linked_list.c
@@ -10,10 +10,10 @@ thread.o: thread.c thread.h linked_list.o
 	$(CC) -g -c thread.c
 
 test01: test01.c thread.o
-	$(CC) -g -c test01.c -o test01 thread.o linked_list.o
+	$(CC) -g test01.c -o test01 thread.o linked_list.o
 
 test01x: test01x.c thread.o
-	$(CC) -g -c test01x.c -o test01x thread.o linked_list.o
+	$(CC) -g test01x.c -o test01x thread.o linked_list.o
 
 clean: 
 	rm -rf thread.o test01.o test01x.o
--- thread.c
+++ thread.c
@@ -30,63 +30,100 @@ int compare_tcb(tcb_t *tcb_0, tcb_t *tcb_1) {
 
 void free_nothing(void *thing) {}
 
+tcb_t * t_halt(int terminate) {
+    //get the thread at the head of thr running queue
+    tcb_t * cur_tcb = (tcb_t *) running->value;
+    
+    if (terminate) {
+        //remove this thread from the running queue
+        running = remove_from_linked_list(
+                running, 
+                (void *) cur_tcb, 
+                (compare_f) compare_tcb, 
+                free
+        );
+       
+        //we got rid of the current tcb, so no need to return it
+        return NULL;
+
+    } else {
+        //remove this thread from the running queue
+        running = remove_from_linked_list(
+                running, 
+                (void *) cur_tcb,
+                (compare_f) compare_tcb, 
+                free_nothing
+        );
+        
+        //...and add it to the emd of the ready queue
+        ready = append_to_linked_list(ready, (void *) cur_tcb);
+        
+        return cur_tcb;
+
+        //save the location in the program of the current running thread
+        //getcontext(&cur_tcb->thread_context);
+    }
+}
+
+void t_run_next(start_f start) {
+    //get the next tcb off of the ready queue
+    tcb_t *next_tcb = (tcb_t *) ready->value;
+    ready = remove_from_linked_list(
+            ready, 
+            (void *) next_tcb,
+            (compare_f) compare_tcb, 
+            free_nothing
+    );
+    running = append_to_linked_list(running, (void *) next_tcb);
+
+    //if we aren't given a start function to run, switch to the last context used by this tcb
+    if (NULL == start) {
+        setcontext(&next_tcb->thread_context);
+    //if we are given a start functin, just run it
+    } else {
+        start(next_tcb->thread_id);
+    }
+}
+
 int t_create(start_f start, int thread_id, int priority) {
     tcb_t *new_tcb = malloc(sizeof(tcb_t));
 
     //intialise the fields of the tcb
     new_tcb->thread_id = thread_id;
     new_tcb->thread_priority = priority;
+    
+    t_halt(0);
 
     //save this location in the program
     int flag = 1;
     getcontext(&new_tcb->thread_context);
 
+    //add the new tcb to the front of the ready queue, and run it immediately
+    ready = add_to_linked_list(ready, (void *) new_tcb);
+
     //...then run the start function exactly once
     if (flag) {
         flag = 0;
-        start(thread_id);
+        t_run_next(start);
     }
 }
-
-void t_switch(int terminate) {
-    //only pause this thread if we have another to run
-    if (NULL != ready) {
-        if (terminate) {
-            //remove this thread from the running queue
-            running = remove_from_linked_list(running, (compare_f) compare_tcb, free);
-            
-        } else {
-            //save the location in the program of the current running thread
-            int flag = 1;
-            tcb_t * cur_tcb = (tcb_t *) running->value;
-            getcontext(&cur_tcb->thread_context);
-        
-            //remove this thread from the running queue
-            running = remove_from_linked_list(running, (compare_f) compare_tcb, free_nothing);
-            
-            //...and add it to the emd of the ready queue
-            ready = append_to_linked_list(ready, (void *) cur_tcb);
-        }
-
-        //get the next tcb off of the ready queue
-        tcb_t *next_tcb = (tcb_t *) ready->value;
-        ready = remove_from_linked_list(ready, (compare_f) compare_tcb, free_nothing);
-        running = append_to_linked_list(running, (void *) next_tcb);
-
-        //...then run the next thread exactly once
-        if (flag) {
-            flag = 0;
-            setcontext(&next_tcb->thread_context);
-        }
-    }
-
-}
-
 
 void t_terminate() {
-   t_switch(1); 
+    int flag = 1;
+    t_halt(1);
+
+    if (flag) {
+        flag = 0;
+        t_run_next(NULL);
+    }
 }
 
 void t_yield() {
-    t_switch(0);
+    int flag = 1;
+    t_halt(0);
+
+    if (flag) {
+        flag = 0;
+        t_run_next(NULL);
+    }
 }
